53c9afeb7f141ac532a8babe9aea5e3b14e6bc49
shgysk8zer0 <shgysk8zer0@gmail.com>
2014-09-22 17:01:59 -0700

    Update with time from start of execution

--- resources/cron.php
+++ resources/cron.php
@@ -211,7 +211,7 @@
 			//Update all jobs which completed with new last_run
 			foreach($this->jobs as $job) {
 				$pdo->bind([
-					'time' => date('Y-m-d H:i:s'),
+					'time' => date('Y-m-d H:i:s', $this->time),
 					'function' => $job->function
 				])->execute();
 			}
