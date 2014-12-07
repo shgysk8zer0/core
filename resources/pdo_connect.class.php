5f1977dd861cff1907e97b195598e3265468f71e
Chris Zuber <shgysk8zer0@gmail.com>
2014-12-06 21:03:25 -0800

    Added better error detection, ability to handle multiple formats for creds
    - __construct uses try/catch, with several checks for throwing
    - If file is a string, find its full path in include_path()
    - Can be of types JSON, INI, or XML (defaults to ini if no extension).
    - Use Exceptions instead of magic constants for line, allowing better
    debugging
    - Formatting fixes

--- resources/pdo_connect.class.php
+++ resources/pdo_connect.class.php
@@ -29,7 +29,10 @@
 	class pdo_connect extends \PDO {
 		protected $connect;
 		protected static $instances = [];
-		public $connected;
+		public $connected = false;
+
+		const DEFAULT_SERVER = 'localhost';
+
 
 		/**
 		 * Static class load method
@@ -41,7 +44,7 @@
 		 * @return
 		 */
 
-		public static function load($con = 'connect') {
+		public static function load($con = 'connect.ini') {
 			if(!array_key_exists($con, self::$instances)) {
 				self::$instances[$con] = new self($con);
 			}
@@ -62,35 +65,94 @@
 		 * @example parent::__construct($con)
 		 */
 
-		public function __construct($con = 'connect') {
-			$this->connected = false;
-
-			if(is_string($con)) {
-				$this->connect = (object)parse_ini_file("{$con}.ini");
-			}
-			elseif(is_object($con)) {
-				$this->connect = $con;
-			}
-			elseif(is_array($con)) {
-				$this->connect = (object)$con;
-			}
-
+		public function __construct($con = 'connect.ini') {
 			try{
-				if(isset($this->connect->server) and array_key_exists('SERVER_ADDR', $_SERVER) and $this->connect->server === $_SERVER['SERVER_ADDR']) unset($this->connect->server);
-				if(isset($this->connect->port) and (!isset($this->connect->server) or $this->connect->server === 'localhost')) unset($this->connect->port);
-				if(!(isset($this->connect->user) and isset($this->connect->password))) throw new \Exception('Missing credentials to connect to database');
+				if(is_string($con)) {
+					$ext = strtolower(pathinfo($con, PATHINFO_EXTENSION));
+					if(empty($ext)) {
+						$ext = 'ini';
+						$con = $con . 'ini';
+					}
+					$con = stream_resolve_include_path($con);
+					if(is_string($con) and is_readable($con)) {
+						switch($ext) {
+							case 'ini': {
+								$this->connect = (object)parse_ini_file($con);
+							} break;
+							case 'json': {
+								$this->connect = json_decode(file_get_contents($con));
+							} break;
+							case 'xml': {
+								$this->connect = simplexml_load_file($con);
+							}
+							default: {
+								throw new \Exception('Unsupported format for credentials');
+							}
+						}
+					}
+					else {
+						throw new \Exception('Unable to find or read credentials file' . print_r($con, true));
+					}
+				}
+				elseif(is_object($con)) {
+					$this->connect = $con;
+				}
+				elseif(is_array($con)) {
+					$this->connect = (object)$con;
+				}
+
+				if(is_null($this->connect) or !is_object($this->connect)) {
+					throw new \Exception('Unable to parse credentials.');
+				}
+				elseif(is_null($this->connect->user)) {
+					throw new \Exception('User not given in credentials');
+				}
+				elseif(is_null($this->connect->password)) {
+					throw new \Exception('Password not given in credentials');
+				}
+
+				if(
+					isset($this->connect->server)
+					and array_key_exists('SERVER_ADDR', $_SERVER)
+					and $this->connect->server === $_SERVER['SERVER_ADDR']
+				) {
+					unset($this->connect->server);
+				}
+
+				if(
+					isset($this->connect->port)
+					and (
+						!isset($this->connect->server)
+						or $this->connect->server === $this::DEFAULT_SERVER
+					)
+				) {
+					unset($this->connect->port);
+				}
 				$connect_string = (isset($this->connect->type)) ? "{$this->connect->type}:" : 'mysql:';
 				$connect_string .= (isset($this->connect->database)) ?  "dbname={$this->connect->database}" : "dbname={$this->connect->user}";
-				if(isset($this->connect->server)) $connect_string .= ";host={$this->connect->server}";
-				if(isset($this->connect->port) and isset($this->connect->server) and $this->connect->server !== 'localhost') $connect_string .= ";port={$this->connect->port}";
-				parent::__construct($connect_string, $this->connect->user, $this->connect->password);
+
+				if(isset($this->connect->server)) {
+					$connect_string .= ";host={$this->connect->server}";
+				}
+
+				if(
+					isset($this->connect->port)
+					and isset($this->connect->server)
+					and $this->connect->server !== $this::DEFAULT_SERVER
+				) {
+					$connect_string .= ";port={$this->connect->port}";
+				}
+
+				parent::__construct(
+					$connect_string,
+					"{$this->connect->user}",
+					"{$this->connect->password}"
+				);
 				$this->connected = true;
 			}
+
 			catch(\Exception $e) {
-				if(!isset($connect_string)) {
-					$connect_string = 'Connect String not set';
-				}
-				$this->log(__METHOD__, __LINE__, $connect_string . PHP_EOL . $e->getMessage());
+				$this->log(__METHOD__, $e->getLine(),$e->getMessage());
 			}
 		}
 
@@ -104,7 +166,11 @@
 		 */
 
 		private function log($method = null, $line = null, $message = '') {
-			file_put_contents(BASE . '/' . __CLASS__ . '.log', "Error in $method in line $line: $message" . PHP_EOL, FILE_APPEND | LOCK_EX);
+			file_put_contents(
+				BASE . DIRECTORY_SEPERATOR . __CLASS__ . '.log',
+				"Error in $method in line $line: $message" . PHP_EOL,
+				FILE_APPEND | LOCK_EX
+			);
 		}
 
 		/**
@@ -143,19 +209,27 @@
 
 		public function dump($filename = null) {
 			if(is_null($filename)) {
-				$filename = BASE . '/' . $this->connect->database;
+				$filename = BASE . DIRECTORY_SEPARATOR . $this->connect->database;
 			}
 
-			if((file_exists("{$filename}.sql") and is_writable("{$filename}.sql")) or (!file_exists("{$filename}.sql") and is_writable(BASE))) {
+			if(
+				(
+					file_exists("{$filename}.sql")
+					and is_writable("{$filename}.sql")
+				) or (
+					!file_exists("{$filename}.sql")
+					and is_writable(BASE)
+				)
+			) {
 				$command = "mysqldump -u {$this->connect->user} -p" . escapeshellcmd($this->connect->password);
 
-				if(isset($this->connect->server) and $this->connect->server !== 'localhost') {
+				if(isset($this->connect->server) and $this->connect->server !== $this::DEFAULT_SERVER) {
 					$command .= " -h {$this->connect->server}";
 				}
 
 				$command .= " {$this->connect->database} > {$filename}.sql";
 
-				exec($command);
+				exec(escapeshellcmd($command));
 				return true;
 			}
 			else {
