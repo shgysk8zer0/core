f555eb4420b73909d298b37ddc8aa8674988265e
Chris Zuber <shgysk8zer0@gmail.com>
2015-01-09 01:18:50 -0800

    Correctly set BASE & include_path
    Not sure how this will work out on Travis-CI since it will somewhat be
    using a directory above the repository, though not touching anything.

--- tests/unit.php
+++ tests/unit.php
@@ -1,7 +1,7 @@
 <?php
 	namespace shgysk8zer0\Core
 	{
-		define('BASE', dirname(__DIR__));
+		define('BASE', dirname(dirname(dirname(__DIR__))));
 		set_include_path(BASE);
 		spl_autoload_extensions('.php');
 		spl_autoload_register('spl_autoload');
