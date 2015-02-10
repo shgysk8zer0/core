4eabfabd6a8db72e0f6ae88cd8593bc145339004
Chris Zuber <shgysk8zer0@gmail.com>
2015-02-09 22:19:05 -0800

    Use Core_API resources & add new methods
    * Use traits & interfaces from Core_API
    * Added AJAXError method and removed trigger method (just use
    trigger_error)
    * Added documentation

--- errors.php
+++ errors.php
@@ -1,11 +1,11 @@
 <?php
 namespace shgysk8zer0\Core;
 
-class Errors
+class Errors implements \shgysk8zer0\Core_API\Interfaces\Errors
 {
-	use \shgysk8zer0\Core\Traits\Errors;
+	use \shgysk8zer0\Core_API\Traits\Errors;
 
-	const DEFAULT_METHOD = 'printError';
+	const DEFAULT_METHOD = 'reportError';
 
 	public static $LOG_DIR = 'logs',
 		$LOG_FILE = 'errors.log';
@@ -16,7 +16,7 @@ class Errors
 	 * @param string $method Name of method to call on errors
 	 * @param int    $level  E_* constant(s)
 	 */
-	public function __construct($method = 'printError', $level = null)
+	public function __construct($method = self::DEFAULT_METHOD, $level = null)
 	{
 		if (! is_int($level)) {
 			$level = error_reporting();
@@ -27,11 +27,69 @@ class Errors
 		set_error_handler([$this, $method], $level);
 	}
 
-	final public static function printError($level, $message, $file, $line, $scope)
+	/**
+	 * Prints an error
+	 *
+	 * @param int    $level   Any of the error levels (E_*)
+	 * @param string $message Message given with the error
+	 * @param string $file    File generating the error
+	 * @param int    $line    Line on which the error occured
+	 * @param array  $context All set variables in scope
+	 * @return void
+	 */
+	final public static function reportError(
+		$level,
+		$message,
+		$file,
+		$line,
+		array $context = array()
+	)
 	{
-		echo static::errorToException($level, $message, $file, $line, $scope) . PHP_EOL;
+		echo static::errorToException($level, $message, $file, $line, $context) . PHP_EOL;
 	}
 
+	/**
+	 * Sends an error to console.error
+	 *
+	 * @param int    $level   Any of the error levels (E_*)
+	 * @param string $message Message given with the error
+	 * @param string $file    File generating the error
+	 * @param int    $line    Line on which the error occured
+	 * @param array  $context All set variables in scope
+	 * @return void
+	 */
+	final static public function AJAXError(
+		$level,
+		$message,
+		$file,
+		$line,
+		array $context = array()
+	)
+	{
+		header('Content-Type: application/json');
+		$e = static::errorToException($level, $message, $file, $line, $context);
+		exit(json_encode([
+			'error' => [
+				'level' => static::errorLevelAsString($level),
+				'file' => $e->getFile(),
+				'line' => $e->getLine(),
+				'message' => $e->getMessage(),
+				'trace' => $e->getTraceAsString(),
+				'class' => get_class($e)
+			]
+		]));
+	}
+
+	/**
+	 * Saves an error to file
+	 *
+	 * @param int    $level   Any of the error levels (E_*)
+	 * @param string $message Message given with the error
+	 * @param string $file    File generating the error
+	 * @param int    $line    Line on which the error occured
+	 * @param array  $context All set variables in scope
+	 * @return void
+	 */
 	final public static function logError($level, $message, $file, $line, $scope)
 	{
 		file_put_contents(
@@ -39,10 +97,5 @@ class Errors
 			static::errorToException($level, $message, $file, $line, $scope) . PHP_EOL,
 			LOCK_EX | FILE_APPEND
 		);
-	}
-
-	final static public function trigger($message, $level = E_USER_NOTICE)
-	{
-		trigger_error($message, $level);
 	}
 }
