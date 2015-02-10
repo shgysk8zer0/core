2e8ab8784ed0637de0005028e575553a2667777b
Chris Zuber <shgysk8zer0@gmail.com>
2015-02-09 21:54:59 -0800

    shgysk8zer0\Core\Errors::__construct now sets error_handler
    Takes an optional method and error level and sets that method as error
    handler.

--- errors.php
+++ errors.php
@@ -5,8 +5,27 @@ class Errors
 {
 	use \shgysk8zer0\Core\Traits\Errors;
 
+	const DEFAULT_METHOD = 'printError';
+
 	public static $LOG_DIR = 'logs',
 		$LOG_FILE = 'errors.log';
+
+	/**
+	 * Sets $this::{$method} as error handler
+	 *
+	 * @param string $method Name of method to call on errors
+	 * @param int    $level  E_* constant(s)
+	 */
+	public function __construct($method = 'printError', $level = null)
+	{
+		if (! is_int($level)) {
+			$level = error_reporting();
+		}
+		if ( ! is_string($method) or ! method_exists($this, $method)) {
+			$method = $this::DEFAULT_METHOD;
+		}
+		set_error_handler([$this, $method], $level);
+	}
 
 	final public static function printError($level, $message, $file, $line, $scope)
 	{
