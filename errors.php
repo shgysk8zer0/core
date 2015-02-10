57189adb1e2eb657dc4201a9f768ee341db00151
Chris Zuber <shgysk8zer0@gmail.com>
2015-02-09 22:24:33 -0800

    More documentation
    Added top documentation (copyright, author, etc) and ducomented class
    variables.

--- errors.php
+++ errors.php
@@ -1,14 +1,45 @@
 <?php
+/**
+* @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace shgysk8zer0\Core;
 
+/**
+ * Provides easy implementation of error reporting though several methods
+ */
 class Errors implements \shgysk8zer0\Core_API\Interfaces\Errors
 {
 	use \shgysk8zer0\Core_API\Traits\Errors;
 
 	const DEFAULT_METHOD = 'reportError';
 
-	public static $LOG_DIR = 'logs',
-		$LOG_FILE = 'errors.log';
+	/**
+	 * Directory to use for logError
+	 * @var string
+	 */
+	public static $LOG_DIR = 'logs';
+
+	/**
+	 * Filename to use for logError
+	 * @var string
+	 */
+	public static $LOG_FILE = 'errors.log';
 
 	/**
 	 * Sets $this::{$method} as error handler
@@ -22,7 +53,7 @@ class Errors implements \shgysk8zer0\Core_API\Interfaces\Errors
 			$level = error_reporting();
 		}
 		if ( ! is_string($method) or ! method_exists($this, $method)) {
-			$method = $this::DEFAULT_METHOD;
+			$method = self::DEFAULT_METHOD;
 		}
 		set_error_handler([$this, $method], $level);
 	}
