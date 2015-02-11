7389159807b5c40e5482ed8b35bd3e293d08ad3f
Chris Zuber <shgysk8zer0@gmail.com>
2015-02-10 16:49:08 -0800

    Refactor Login class
    * Now uses API Traits, Abstracts, and Interfaces instead of extending
    PDO
    * Updated documentation
    * Fixed indentation
    * All method names are now camelCase
    * Make use of several traits and interfaces
    * underscore_separated methods moved to a legacy trait

--- login.php
+++ login.php
@@ -1,169 +1,146 @@
 <?php
-	namespace shgysk8zer0\Core;
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\Core
+ * @version 1.0.0
+ * @copyright 2015, Chris Zuber
+ * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Class to handle login or create new users from form submissions or $_SESSION
+ * Can check login role as well (new, user, admin, etc)
+ */
+
+namespace shgysk8zer0\Core;
+
+use \shgysk8zer0\Core_API as API;
+
+/**
+ * Checks login status, creates or logs in with an array, and can logout as well.
+ */
+class login extends API\Abstracts\PDO_Connect
+implements API\Interfaces\PDO, API\Interfaces\Magic_Methods
+{
+	use API\Traits\Singleton;
+	use API\Traits\Magic_Methods;
+	use API\Traits\PDO;
+	use Traits\Legacy_Login;
+
+	const STM_CLASS = 'PDOStatement';
+	const DEFAULT_CON = 'connect.json';
+	const MAGIC_PROPERTY = 'data';
+	const USER_TABLE = '`users`';
 
 	/**
-	 * Class to handle login or create new users from form submissions or $_SESSION
-	 * Can check login role as well (new, user, admin, etc)
-	 *
-	 * @author Chris Zuber <shgysk8zer0@gmail.com>
-	 * @package shgysk8zer0\Core
-	 * @version 1.0.0
-	 * @uses /classes/PDO.php
-	 * @copyright 2014, Chris Zuber
-	 * @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
-	 * This program is free software; you can redistribute it and/or
-	 * modify it under the terms of the GNU General Public License
-	 * as published by the Free Software Foundation, either version 3
-	 * of the License, or (at your option) any later version.
-	 *
-	 * This program is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	 * GNU General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU General Public License
-	 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	 *
-	 * @var array $data
-	 * @var login $instance
+	 * Array to store login data
+	 * @var array
 	 */
-	class login extends PDO
+	private $data = [];
+
+	public function __construct($con = null)
 	{
-		public $data = [];
-		protected static $instance = null;
+		parent::connect(
+			$con,
+			[
+				self::ATTR_STATEMENT_CLASS => ['\\' . __NAMESPACE__ . '\\' . self::STM_CLASS]
+			]
+		);
+	}
 
-		/**
-		 * Static load function avoids creating multiple instances/connections
-		 * It checks if an instance has been created and returns that or a new instance
-		 *
-		 * @param string $ini (ini file to use for database connection configuration)
-		 * @return self
-		 * @example $login = _login::load
-		 */
-		public static function load($ini = 'connect.json')
-		{
-			if (is_null(self::$instance)) {
-				self::$instance = new self($ini);
-			}
+	/**
+	 * Creates new user using an array passed as source. Usually $_POST or $_SESSION
+	 *
+	 * @param array $source
+	 * @return bool
+	 * @example $login->create_from($_POST|$_GET|$_REQUEST|array())
+	 */
+	public function createFrom(array $source = array())
+	{
+		if (array_key_exists('user', $source) and array_key_exists('password', $source)) {
+			$keys = array_map(function($key) {
+				return preg_replace('/\W/', null, $key);
+			}, array_keys($source));
 
-			return self::$instance;
-		}
+			$source = array_combine($keys, array_values($source));
 
-		/**
-		 * Gets database connection info from /connect.ini (stored in $site)
-		 * Uses that data to create a new PHP Data Object
-		 *
-		 * @param string $ini (ini file to use for database connection configuration)
-		 * @example $login = new login()
-		 * @todo Use static parent::load() instead, but this causes errors
-		 */
-		public function __construct($ini = 'connect.json')
-		{
-			parent::__construct($ini);	//login extends PDO, so create new instance of parent.
-
-			$this->data = array(
-				'user' => null,
-				'password' => null,
-				'role' => null,
-				'logged_in' => false
+			$source['password'] = password_hash(
+				$source['password'],
+				PASSWORD_DEFAULT
 			);
-		}
 
-		/**
-		 * Creates new user using an array passed as source. Usually $_POST or $_SESSION
-		 *
-		 * @param array $source
-		 * @return bool
-		 * @example $login->create_from($_POST|$_GET|$_REQUEST|array())
-		 */
-		public function create_from(array $source)
-		{
-			if (array_keys_exist('user', 'password', $source)) {
-				$keys = array_map(function($key) {
-					return preg_replace('/\W/', null, $key);
-				}, array_keys($source));
-
-				$source = array_combine($keys, array_values($source));
-
-				$source['password'] = password_hash(
-					$source['password'],
-					PASSWORD_DEFAULT
-				);
-
-				return $this->prepare(
-					"INSERT INTO `users` (
-						`" . join('`, `', $keys) . "`
-					) VALUES ("
-						. join(', ', array_map(function($key) {
-							return ':' . $key;
-						}, $keys)) . "
-					)"
-				)->bind(
-					$source
-				)->execute();
-			} else {
-				return false;
-			}
-		}
-
-		/**
-		 * Intended to find login info from $_COOKIE, $_SESSION, or $_POST
-		 *
-		 * @param array $source
-		 * @return void
-		 * @example $login->login_with($_POST|$_GET|$_REQUEST|$_SESSION|array())
-		 */
-		public function login_with(array $source)
-		{
-			if (array_keys_exist('user', 'password', $source)) {
-				array_walk($source, 'trim');
-				$results = $this->prepare(
-					"SELECT *
-					FROM `users`
-					WHERE `user` = :user
-					LIMIT 1"
-				)->bind([
-					'user' => $source['user']
-				])->execute()->get_results(0);
-
-				if (password_verify(
-					$source['password'],
-					$results->password
-				) and $results->role !== 'new') {
-					$results->logged_in = true;
-					$this->data = array_merge(
-						$this->data,
-						get_object_vars($results)
-					);
-					return true;
-				}
-			}
+			return $this->prepare(
+				"INSERT INTO " . $this::USER_TABLE . "(
+					`" . join('`, `', $keys) . "`
+				) VALUES ("
+					. join(', ', array_map(function($key) {
+						return ':' . $key;
+					}, $keys)) . "
+				)"
+			)->bind(
+				$source
+			)->execute();
+		} else {
 			return false;
 		}
-
-		/**
-		 * Undo the login. Destroy it. Removes session and cookie. Sets logged_in to false
-		 *
-		 * @param void
-		 * @return void
-		 */
-		public function logout()
-		{
-			$this->data = array_combine(
-				array_keys($this->data),
-				array_pad([], count($this->data), null)
-			);
-		}
-
-		/**
-		 * Prints out class information using print_r
-		 * wrapped in <pre> and <code>
-		 *
-		 * @param void
-		 * @return void
-		 */
-		public function debug()
-		{
-			debug($this);
-		}
 	}
+
+	/**
+	 * Intended to find login info from $_COOKIE, $_SESSION, or $_POST
+	 *
+	 * @param array $source
+	 * @return void
+	 * @example $login->login_with($_POST|$_GET|$_REQUEST|$_SESSION|array())
+	 */
+	public function loginWith(array $source = array())
+	{
+		if (array_key_exists('user', $source) and array_key_exists('password', $source)) {
+			array_walk($source, 'trim');
+			$results = $this->prepare(
+				"SELECT *
+				FROM " . $this::USER_TABLE . "
+				WHERE `user` = :user
+				LIMIT 1"
+			)->bind([
+				'user' => $source['user']
+			])->execute()->getResults(0);
+
+			if (password_verify(
+				$source['password'],
+				$results->password
+			) and $results->role !== 'new') {
+				$results->logged_in = true;
+				$this->data = array_merge(
+					$this->data,
+					get_object_vars($results)
+				);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Undo the login. Destroy it. Removes session and cookie. Sets logged_in to false
+	 *
+	 * @param void
+	 * @return void
+	 */
+	public function logout()
+	{
+		$this->data = array_combine(
+			array_keys($this->data),
+			array_pad([], count($this->data), null)
+		);
+	}
+}
