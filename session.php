d717eb9f37ed7699fc50da7d47fedfbf309bfe36
Chris Zuber <shgysk8zer0@gmail.com>
2015-02-23 11:10:33 -0800

    Fix missing semicolon
    Silly typos breaking everything...

--- session.php
+++ session.php
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-namespace shgysk8zer0\Core
+namespace shgysk8zer0\Core;
 
 use \shgysk8zer0\Core_API as API;
 
