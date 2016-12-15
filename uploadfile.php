06d9c1f15385b4fef8f0df210d36f5c605458f47
Chris Zuber <shgysk8zer0@gmail.com>
2016-12-15 15:37:46 -0800

    Fix typo in validating files

--- uploadfile.php
+++ uploadfile.php
@@ -22,101 +22,101 @@ namespace shgysk8zer0\Core;
 /**
  * Class for validating and working with uploaded files
  */
-class UploadFile extends \ArrayObject
-{
-	/**
-	 * Create a new instance
-	 * @param string $key Key, as found in `$_FILES`
-	 */
-	public function __construct($key)
-	{
-		if (array_key_exists($key, $_FILES)) {
-			parent::__construct($_FILES[$key], self::ARRAY_AS_PROPS);
-			if (! isset($this->name, $this->size, $this->error, $this->tmp_name)) {
-				throw new \InvalidArgumentException("$key is not a valid file upload.");
-			} elseif (!is_uploaded_file($this->tmp_name)) {
-				throw new \InvalidArgumentException('File is not an uploaded file.');
-			} elseif (!$this->checkFile()) {
-				trigger_error("{$this->name} does not match uploaded file.", E_USER_ERROR);
-			} else {
-				$this->_checkError();
-			}
-		} else {
-			throw new \InvalidArgumentException("$key not found in _FILES");
-		}
-	}
+ class UploadFile extends \ArrayObject
+ {
+ 	/**
+ 	 * Create a new instance
+ 	 * @param string $key Key, as found in `$_FILES`
+ 	 */
+ 	public function __construct($key)
+ 	{
+ 		if (array_key_exists($key, $_FILES)) {
+ 			parent::__construct($_FILES[$key], self::ARRAY_AS_PROPS);
+ 			if (! isset($this->name, $this->size, $this->error, $this->tmp_name)) {
+ 				throw new \InvalidArgumentException("$key is not a valid file upload.");
+ 			} elseif (!is_uploaded_file($this->tmp_name)) {
+ 				throw new \InvalidArgumentException('File is not an uploaded file.');
+ 			} elseif (!$this->_checkFile()) {
+ 				trigger_error("{$this->name} does not match uploaded file.", \E_USER_ERROR);
+ 			} else {
+ 				$this->_checkError();
+ 			}
+ 		} else {
+ 			throw new \InvalidArgumentException("$key not found in _FILES");
+ 		}
+ 	}
 
-	/**
-	 * Returns JSON encoded version of file data
-	 * @param void
-	 * @return string JSON string
-	 */
-	public function __toString()
-	{
-		return json_encode($this->getArrayCopy());
-	}
+ 	/**
+ 	 * Returns JSON encoded version of file data
+ 	 * @param void
+ 	 * @return string JSON string
+ 	 */
+ 	public function __toString()
+ 	{
+ 		return json_encode($this->getArrayCopy());
+ 	}
 
-	/**
-	 * Moves uploaded file into $dir directory
-	 * @param  string  $dir Directory to move the file into
-	 * @return boolean      Whether or not the move was successful
-	 */
-	public function saveTo($dir)
-	{
-		if (!is_dir($dir)) {
-			mkdir($dir, '0777', true);
-		}
-		return move_uploaded_file(
-			$this->tmp_name,
-			DIRECTORY_SEPARATOR . trim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $this->name
-		);
-	}
+ 	/**
+ 	 * Moves uploaded file into $dir directory
+ 	 * @param  string  $dir Directory to move the file into
+ 	 * @return boolean      Whether or not the move was successful
+ 	 */
+ 	public function saveTo($dir)
+ 	{
+ 		if (!is_dir($dir)) {
+ 			mkdir($dir, '0777', true);
+ 		}
+ 		return move_uploaded_file(
+ 			$this->tmp_name,
+ 			DIRECTORY_SEPARATOR . trim($dir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $this->name
+ 		);
+ 	}
 
-	/**
-	 * Checks whether the uploaded file matches MIME and size
-	 * @param void
-	 * @return boolean Whether or not they match
-	 */
-	private function _checkFile()
-	{
-		$info = new \Finfo();
-		return $info->file($this->tmp_name, FILEINFO_MIME_TYPE) === $this->type
-			and filesize($this->tmp_name) === $this->size;
-	}
+ 	/**
+ 	 * Checks whether the uploaded file matches MIME and size
+ 	 * @param void
+ 	 * @return boolean Whether or not they match
+ 	 */
+ 	private function _checkFile()
+ 	{
+ 		$info = new \Finfo();
+ 		return $info->file($this->tmp_name, \FILEINFO_MIME_TYPE) === $this->type
+ 			and filesize($this->tmp_name) === $this->size;
+ 	}
 
-	/**
-	 * Checks for upload errors. Triggers error if not `UPLOAD_ERR_OK`.
-	 * @param void
-	 * @return void
-	 */
-	private function _checkError()
-	{
-		switch($this->error) {
-			case UPLOAD_ERR_OK:
-				break;
-			case UPLOAD_ERR_INI_SIZE:
-				trigger_error("{$this->name} exceeds max upload size set in php.ini");
-				break;
-			case UPLOAD_ERR_FORM_SIZE:
-				trigger_error("{$this->name} exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.");
-				break;
-			case UPLOAD_ERR_PARTIAL:
-				trigger_error("{$this->name} was only partially uploaded.");
-				break;
-			case UPLOAD_ERR_NO_FILE:
-				trigger_error("No file was uploaded");
-				break;
-			case UPLOAD_ERR_NO_TMP_DIR:
-				trigger_error("Missing a temporary folder. Could not upload {$this->name}.");
-				break;
-			case UPLOAD_ERR_CANT_WRITE:
-				trigger_error("Failed to write {$this->name} to disk.");
-				break;
-			case UPLOAD_ERR_EXTENSION:
-				trigger_error('A PHP extension stopped the file upload.');
-				break;
-			default:
-				trigger_error('Unknown file upload error.');
-		}
-	}
-}
+ 	/**
+ 	 * Checks for upload errors. Triggers error if not `UPLOAD_ERR_OK`.
+ 	 * @param void
+ 	 * @return void
+ 	 */
+ 	private function _checkError()
+ 	{
+ 		switch($this->error) {
+ 			case UPLOAD_ERR_OK:
+ 				break;
+ 			case UPLOAD_ERR_INI_SIZE:
+ 				trigger_error("{$this->name} exceeds max upload size set in php.ini");
+ 				break;
+ 			case UPLOAD_ERR_FORM_SIZE:
+ 				trigger_error("{$this->name} exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.");
+ 				break;
+ 			case UPLOAD_ERR_PARTIAL:
+ 				trigger_error("{$this->name} was only partially uploaded.");
+ 				break;
+ 			case UPLOAD_ERR_NO_FILE:
+ 				trigger_error("No file was uploaded");
+ 				break;
+ 			case UPLOAD_ERR_NO_TMP_DIR:
+ 				trigger_error("Missing a temporary folder. Could not upload {$this->name}.");
+ 				break;
+ 			case UPLOAD_ERR_CANT_WRITE:
+ 				trigger_error("Failed to write {$this->name} to disk.");
+ 				break;
+ 			case UPLOAD_ERR_EXTENSION:
+ 				trigger_error('A PHP extension stopped the file upload.');
+ 				break;
+ 			default:
+ 				trigger_error('Unknown file upload error.');
+ 		}
+ 	}
+ }
